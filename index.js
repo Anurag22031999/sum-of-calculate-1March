ce363e2b8efdaa51f9ea80ed3f695cc7bd655c62
Anurag22031999 <96306573+Anurag22031999@users.noreply.github.com>
2022-03-02 22:10:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 function Calculate() {
-
   var num1 = document.getElementById("num1").value;
+
   var num2 = document.getElementById("num2").value;
-  
-  num1_parsed = parseInt(num1);    
-  num2_parsed = parseInt(num2);
-  
-  
- //console.log((!(isNaN(num1_parsed))).toString() + " " + num1_parsed);
- //console.log((!(isNaN(num2_parsed))).toString() +  " " + num2_parsed);
-
-  if (!(isNaN(num1_parsed)) && !(isNaN(num2_parsed))){
-  var total = num1_parsed + num2_parsed;
-  alert("The total sum of your numbers are: " + total);
-  
-  } else {
-alert("Wrong input!!!");
-    return false;        
+  if( !num1 || !num2 ){
+     alert("please enter values for num1 and num2");
+  } 
+  var total = eval(`${num1}+${num2}`);
+  if( isNaN(total) ){
+     alert("Wrong input!!!");
+     return false;    
   }
-
-
+  alert("The total sum of your numbers are: " + total);
 }
